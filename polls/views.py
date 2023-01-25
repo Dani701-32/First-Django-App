1ded4e9224b1998b46ee0975f26ab042a7f97e01
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2023-01-25 16:19:13 -0300

    detail view update

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -11,7 +11,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
