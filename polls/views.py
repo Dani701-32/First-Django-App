a63b58c8053beb465463954593ca6bf9eae2086a
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2023-01-25 16:11:33 -0300

    preparing index view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list, }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
